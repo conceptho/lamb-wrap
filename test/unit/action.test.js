c1ccde1d4d0b5ebb369010d6f1eb19cc5a1ad527
Lucianopf <lucianopf@outlook.com>
2016-07-12 18:16:24 -0300

    quickfixes at names and definitions

--- test/unit/action.test.js
+++ test/unit/action.test.js
@@ -3,25 +3,25 @@
 let Action = require('../../src/action')
 
 describe('Action Class', () => {
-  describe('#createAction', () => {
+  describe('.create', () => {
     it('Empty config should throw error', (done) => {
       (() => Action.create()).should.throw(Error)
       return done()
     })
     it('Missing schema (required argument) should throw error', (done) => {
-      (() => Action.create({event: {}, context: {}, operation: '', body: () => null})).should.throw(Error)
+      (() => Action.create({operation: '', body: () => null})).should.throw(Error)
       return done()
     })
     it('Invalid operation constant sent by user should throw error', (done) => {
-      (() => Action.create({event: {}, context: {}, schema: '', operation: 'GIMME_MY_LAMB', body: () => null})).should.throw(Error)
+      (() => Action.create({schema: '', operation: 'GIMME_MY_LAMB', body: () => null})).should.throw(Error)
       return done()
     })
     it('Valid default action should create an action', (done) => {
-      (() => Action.create({event: {}, context: {}, schema: '', operation: 'UPDATE', body: () => null})).should.not.throw(Error)
+      (() => Action.create({schema: '', operation: Action.DELETE, body: () => null})).should.not.throw(Error)
       return done()
     })
     it('Action can have custom attributes', (done) => {
-      let actualAction = Action.create({event: {}, context: {}, schema: '', operation: Action.UPDATE, body: () => null, description: 'Desc goes here'})
+      let actualAction = Action.create({schema: '', operation: Action.UPDATE, body: () => null, description: 'Desc goes here'})
       actualAction.should.have.property('description', 'Desc goes here')
       return done()
     })
