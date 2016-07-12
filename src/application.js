4e6f9510ae411c7a561c4806adb2011298aeb033
Lucianopf <lucianopf@outlook.com>
2016-07-12 18:16:39 -0300

    Application now has handler that returns a new function

--- src/application.js
+++ src/application.js
@@ -5,11 +5,22 @@ let AccessRules = require('./access-rules')
 
 let application = {}
 
+application.handler = (action) => {
+  if (!action) {
+    throw new Error('Invalid action as input')
+  }
+  return (event, context) => {
+    action.event = event
+    action.context = context
+    return application.run(action)
+  }
+}
+
 application.run = (action) => {
-  let identity = Authenticator.getIdentify(action)
+  let identity = Authenticator.getIdentity(action)
   let model = action.loadModel()
   AccessRules.checkAccess(identity, action)
-  action
+  return action
     .filterInput()
     .execute(identity, model)
     .filterOutput()
