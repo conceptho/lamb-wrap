e622495226b91e435b7e5a54e0d49d777ced4cd7
Lucianopf <lucianopf@outlook.com>
2016-07-14 16:43:05 -0300

    Fixing application tests

--- test/unit/application.test.js
+++ test/unit/application.test.js
@@ -2,6 +2,7 @@
 
 let Action = require('../../src/action')
 let Application = require('../../src/application')
+let UserSchema = require('../mocks/user.schema')
 
 describe('Application Class', () => {
   describe('.run', () => {
@@ -9,11 +10,10 @@ describe('Application Class', () => {
       (() => Application.run(Action.create({schema: 'User', operation: Action.DELETE, body: () => null}))).should.throw(Error)
       return done()
     })
-    // Not working yet because the application is not fully implemented
-    // it('Should work when runing with a valid action', (done) => {
-    //   (() => Application.run(Action.create({event: {}, context: {success: () => null}, schema: 'User', operation: Action.DELETE, body: () => null}))).should.not.throw(Error)
-    //   return done()
-    // })
+    it('Should work when runing with a valid action', (done) => {
+      (() => Application.run(Action.create({event: {}, context: {success: () => null}, schema: UserSchema, operation: Action.CREATE, body: () => null}))).should.not.throw(Error)
+      return done()
+    })
   })
   describe('.handler', () => {
     it('Should not work if there are problems at action creation', (done) => {
